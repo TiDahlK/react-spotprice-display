b96adef91a9c9ebb350475c981ab3d9902876f5c
Tim Dahl K <ent@live.se>
2025-03-17 10:59:03 +0100

    feat: add header for energyChart section

--- ui/src/components/card/Card.jsx
+++ ui/src/components/card/Card.jsx
@@ -6,21 +6,22 @@ import EnergyChart from "../chart/EnergyChart";
 function Card({ priceArea, high, low, average, energyMix }) {
   return (
     <>
-      <div>
-        <div className="card__container">
-          <h2>
-            <FontAwesomeIcon icon="fa-solid fa-bolt fa-align-center" />{" "}
-            {priceArea} - {average.price}
-          </h2>
-          <div className="card__section">
-            <h3>
-              <FontAwesomeIcon icon="fa-solid fa-circle-arrow-up fa-align-center" />
-              Högst - {high.price} {high.timespan}
-            </h3>
-            <h3>
-              <FontAwesomeIcon icon="fa-solid fa-circle-arrow-down" />
-              Lägst - {low.price} {low.timespan}
-            </h3>
+      <div className="card__container">
+        <h2>
+          <FontAwesomeIcon icon="fa-solid fa-bolt fa-align-center" />{" "}
+          {priceArea} - {average.price}
+        </h2>
+        <div className="card__section">
+          <h3>
+            <FontAwesomeIcon icon="fa-solid fa-circle-arrow-up fa-align-center" />
+            Högst - {high.price} {high.timespan}
+          </h3>
+          <h3>
+            <FontAwesomeIcon icon="fa-solid fa-circle-arrow-down" />
+            Lägst - {low.price} {low.timespan}
+          </h3>
+          <div>
+            <h3>Energimix</h3>
             <EnergyChart energyMix={energyMix}></EnergyChart>
           </div>
         </div>
