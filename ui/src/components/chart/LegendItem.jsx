312e0234b52276e10fe8754e9a2b0b69b645095e
Tim Dahl K <ent@live.se>
2025-03-17 10:58:34 +0100

    feat: make legend into its own component

---
+++ ui/src/components/chart/LegendItem.jsx
@@ -0,0 +1,24 @@
+import "./LegendItem.css";
+export const LegendItem = ({ color, label }) => {
+  return (
+    <div className="legend-item">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        viewBox="0 0 18 18"
+        fill="none"
+      >
+        <circle
+          cx="9"
+          cy="9"
+          r="8"
+          fill={color}
+          stroke="#8c8c8c"
+          stroke-width="1"
+        />
+      </svg>
+      <span>{label}</span>
+    </div>
+  );
+};
