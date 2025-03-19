097f4823de798e190f18e9fa7e55500851784631
Tim Dahl K <ent@live.se>
2025-03-19 11:13:07 +0100

    feat: add neon styling to cards and title

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -74,11 +74,12 @@ function App() {
   }, [energyMix]); // Add energyMix as dependency to check if it was fetched
 
   if (error) return <h1>{error}</h1>;
-  if (!spotPrices || !energyMix) return <h1>Laddar in dagens spotpriser...</h1>;
+  if (!spotPrices || !energyMix)
+    return <h1 className="neon-text">Laddar in dagens spotpriser...</h1>;
 
   return (
     <>
-      <h1>Dagens spotpriser</h1>
+      <h1 className="neon-text">Dagens spotpriser</h1>
       <div className="container">
         {Object.entries(spotPrices).map(([area, spotPriceInfo]) => {
           const props = {
