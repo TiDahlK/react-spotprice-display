f5d2fd1e4989fc86487a57c9546e50e50ca93a4b
Tim Dahl K <ent@live.se>
2025-05-09 14:57:09 +0200

    fix: add date to sessionStorage key to prevent data from going stale

--- ui/src/constants/apiSettings.js
+++ ui/src/constants/apiSettings.js
@@ -1,16 +1,18 @@
+const today = new Date().toISOString().split("T")[0];
+
 export const SPOT_PRICE = {
-  SESSION_STORAGE_KEY: "spotPrice",
+  SESSION_STORAGE_KEY: `spotPrice_${today}`,
   ENDPOINT: "getSpotPrice",
 };
 export const ENERGY_MIX = {
-  SESSION_STORAGE_KEY: "energyMix",
+  SESSION_STORAGE_KEY: `energyMix_${today}`,
   ENDPOINT: "getEnergyMix",
 };
 export const EXCHANGE_DATA = {
-  SESSION_STORAGE_KEY: "exchangeData",
+  SESSION_STORAGE_KEY: `exchangeData_${today}`,
   ENDPOINT: "getImportExport",
 };
 export const ANALYSIS = {
-  SESSION_STORAGE_KEY: "analysis",
+  SESSION_STORAGE_KEY: `analysis_${today}`,
   ENDPOINT: "getAnalysis",
 };
